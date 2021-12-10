630faef0cff2d49a0ed1e608a1b3269ccc41bbc5
fabiofa87 <fabiofadev87@gmail.com>
2021-12-10 13:00:50 -0300

    Add endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { criarDocente } from "./endpoints/criarDocente";
 import { getAllTeachers } from "./endpoints/getAllTeachers";
 import app from "./app";
+import { addTeacherClass } from "./endpoints/addTeacherClass";
 
 app.post('/create/teacher', criarDocente);
+app.put('/class/teacher/:id', addTeacherClass)
 app.get('/teachers', getAllTeachers);
